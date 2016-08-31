2b56913b030d6b834da11a8931b348074b753de4
Zhenwei Chen <Zhenwei Chen>
2016-08-30 19:30:18 -0700

    refactor code to avoid global naming collision

--- js/attractions.js
+++ js/attractions.js
@@ -1,12 +1,20 @@
-angular
-    .module('myApp')
-    .directive("attractions",function(){
-      return{
-        restrict:"E",
-        scope:{},
-        controller:function($scope){
-            $scope.attractions = [{name:"a"},{name:"b"},{name:"c"},{name:"d"}];
-        },
-        templateUrl:"attraction.html",
-      };
-    });
+//attractions.js
+
+(function(){
+    'use strict';
+    angular
+        .module('myApp')
+        .directive("attractions",Attractions);
+
+    // define attractions directive
+    function Attractions(){
+        return{
+          restrict:"E",
+          scope:{},
+          controller:function($scope){
+              $scope.attractions = [{name:"a"},{name:"b"},{name:"c"},{name:"d"}];
+          },
+          templateUrl:"attraction.html",
+        };
+    }
+})();
--- js/mapCtrl.js
+++ js/mapCtrl.js
@@ -1,39 +1,42 @@
-angular.module('myApp')
-    .controller('MapController',MapController);
+(function(){
+    angular.module('myApp')
+        .controller('MapController',MapController);
 
-function MapController($scope,$rootScope){
-    var vm = this;
-    mapOptions = {
-        center:{lat:37.397,lng:-121.644},
-        zoom:11
-    };
+    //var map = new google.maps.Map(document.getElementById('map'),mapOptions);
+    function MapController($scope,$rootScope){
+        var vm = this;
+        mapOptions = {
+            center:{lat:37.397,lng:-121.644},
+            zoom:11
+        };
 
-    vm.map = new google.maps.Map(document.getElementById('map'),mapOptions);
-    var infoWindow = new google.maps.InfoWindow({map: map});
+        vm.map = new google.maps.Map(document.getElementById('map'),mapOptions);
+        var infoWindow = new google.maps.InfoWindow({map: vm.map});
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-        infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            m.map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, vm.map.getCenter());
-        });
-    } else {
-    // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, vm.map.getCenter());
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Current Location');
+                vm.map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, vm.map.getCenter());
+            });
+        } else {
+        // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, vm.map.getCenter());
+        }
+
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                'Error: The Geolocation service failed.' :
+                'Error: Your browser doesn\'t support geolocation.');
+        }
     }
-
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-    }
-}
+})();
